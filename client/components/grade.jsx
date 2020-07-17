16d534fc151a790cd863887fd6cde96d5cb3d2d1
julius oh <juliusoh@gmail.com>
2020-07-17 11:23:54 -0700

    grade table

---
+++ client/components/grade.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Grade(props) {
+  return (
+    <tr>
+      <th scope='row'>{props.grade.name}</th>
+      <td>{props.grade.course}</td>
+      <td>{props.grade.grade}</td>
+    </tr>
+  );
+}
+
+export default Grade;
---
+++ client/components/gradetable.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function GradeTable(props) {
+  return (
+    <table className='table table-hover col-8'>
+      <thead className='thead-light'>
+        <tr>
+          <th scope='col'>Name</th>
+          <th scope='col'>Course</th>
+          <th scope='col'>Grade</th>
+          <th scope='col'>Operations</th>
+        </tr>
+      </thead>
+      <tbody>
+
+      </tbody>
+    </table>
+  );
+}
+
+export default GradeTable;
