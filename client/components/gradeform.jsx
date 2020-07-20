11806af64a3f1607355c0d168da677e20280fb9b
julius oh <juliusoh@gmail.com>
2020-07-20 11:42:46 -0700

    handle input change

---
+++ client/components/gradeform.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default class GradeForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      course: '',
+      grade: ''
+    };
+    this.handleFieldChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.name === 'grade' ? parseInt(target.value) : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+  }
+
+}
